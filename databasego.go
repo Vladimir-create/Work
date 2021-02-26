934b313b4e8e33129d1e9f46eacc649d4c1679a6
Vladimir-create <79356244+Vladimir-create@users.noreply.github.com>
2021-02-26 11:46:37 +0200

    Add files via upload

--- databasego.go
+++ databasego.go
@@ -22,6 +22,16 @@ type (
 			PersonalCode string `json:"personalCode"`
 		} `json:"person"`
 	}
+	Student struct {
+		ID string  `json:"id"`
+		Subjects []string `json:"subjects"`
+		Classroom string `json:"classroom"`
+		Person struct {
+			Name string `json:"name"`
+			Surname string `json:"surname"`
+			PersonalCode string `json:"personalCode"`
+		} `json:"person"`
+	}
 	UpdateTeacher struct {
 		T Teacher `json:"data"`
 	}
@@ -34,6 +44,23 @@ type (
 		} `json:"data"`
 	}
 	ReadTeacher struct {
+		Data struct {
+			ID string `json:"id"`
+		} `json:"data"`
+	}
+	//Student
+	UpdateStudent struct {
+		T Student `json:"data"`
+	}
+	CreateStudent struct {
+		T Student `json:"data"`
+	}
+	DeleteStudent struct {
+		Data struct {
+			ID string `json:"id"`
+		} `json:"data"`
+	}
+	ReadStudent struct {
 		Data struct {
 			ID string `json:"id"`
 		} `json:"data"`
@@ -66,6 +93,19 @@ func (t Teacher) GetReadAction() DefinedAction {
 }
 func (t Teacher) GetDeleteAction() DefinedAction {
 	return &DeleteTeacher{}
+}
+//Student
+func (s Student) GetCreateAction() DefinedAction {
+	return &CreateStudent{}
+}
+func (s Student) GetUpdateAction() DefinedAction {
+	return &UpdateStudent{}
+}
+func (s Student) GetReadAction() DefinedAction {
+	return &ReadStudent{}
+}
+func (s Student) GetDeleteAction() DefinedAction {
+	return &DeleteStudent{}
 }
 
 func (action CreateTeacher) Process(){
@@ -105,6 +145,44 @@ func (action DeleteTeacher) Process() {
 	PrintAll(arriPerson)
 }
 
+//Student
+func (action CreateStudent) Process(){
+	fmt.Println("Create Student")
+	arriPerson = append(arriPerson, &action.T)
+	PrintAll(arriPerson)
+}
+
+func (action UpdateStudent) Process() {
+	fmt.Println("Update Student")
+	for i:=0;i<len(arriPerson);i++{	
+		if arriPerson[i].Read(action.T.ID) {
+			arriPerson[i] = &action.T
+		}
+	}
+	PrintAll(arriPerson)
+}
+
+func (action ReadStudent) Process() {
+	fmt.Println("Read student", action.Data.ID)
+	for i:=0;i<len(arriPerson);i++{
+		if arriPerson[i].Read(action.Data.ID) {
+			arriPerson[i].Print()
+		}
+	}
+}
+
+func (action DeleteStudent) Process() {
+	fmt.Println("Student deleted", action.Data.ID)
+	for i:=0;i<len(arriPerson);i++{
+		if arriPerson[i].Read(action.Data.ID) {
+			copy(arriPerson[i:], arriPerson[i+1:])
+			arriPerson[len(arriPerson)-1] = nil
+			arriPerson = arriPerson[:len(arriPerson)-1]
+		}
+	}
+	PrintAll(arriPerson)
+}
+
 func PrintAll(arriPerson []GeneralObject){
 	for i:=0; i<len(arriPerson); i++{
 		arriPerson[i].Print()
@@ -124,7 +202,22 @@ func (i *Teacher) Print(){
 	fmt.Println()
 }
 
+func (i *Student) Print(){
+	fmt.Println("Id:", i.ID)
+	for j:=0; j<len(i.Subjects);j++{
+		fmt.Println("Subjects",j+1, "=", i.Subjects[j])
+	}
+	fmt.Println("Classroom:", i.Classroom)
+	fmt.Println("Name:", i.Person.Name)
+	fmt.Println("Surname:", i.Person.Surname)
+	fmt.Println("PersonalCode:", i.Person.PersonalCode)
+	fmt.Println()
+}
+
 func (i *Teacher) Read(str string) bool{
+	return i.ID == str
+}
+func (i *Student) Read(str string) bool{
 	return i.ID == str
 }
 
@@ -160,6 +253,39 @@ func (action *CreateTeacher) GetFromJSON (rawData []byte) {
 	}
 }
 
+//Student
+func (action *ReadStudent) GetFromJSON (rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (action *DeleteStudent) GetFromJSON (rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (action *UpdateStudent) GetFromJSON (rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (action *CreateStudent) GetFromJSON (rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
 var arriPerson []GeneralObject
 	
 func main() {
@@ -176,6 +302,8 @@ func main() {
 		switch act.ObjName {
 		case "Teacher":
 			obj = &Teacher{}
+		case "Student":
+			obj = &Student{}
 		}
 		switch act.Action {
 		case "create":
